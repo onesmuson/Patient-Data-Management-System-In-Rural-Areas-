5ef47b6a22a9bccb82939889849e4b8be4fee94a
onesmuson <masooonesmus1@gmail.com>
2025-11-21 12:15:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_secret_key')
+    # Render provides a DATABASE_URL environment variable for Postgres
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL',
         'postgresql://username:password@localhost:5432/pdms_db'
