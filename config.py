a5911e715848fc3f467bc4cfc1b74dac5a3f02b4
onesmuson <masooonesmus1@gmail.com>
2025-11-23 00:23:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,13 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "your_default_secret_key")
 
     # MySQL database settings
-    MYSQL_USER = "Onesmuson"
-    MYSQL_PASSWORD = "Admin123"  # your new MySQL password
+    MYSQL_USER = "Onesmuson"  # PythonAnywhere username
+    MYSQL_PASSWORD = "Admin123"  # Your MySQL password
     MYSQL_HOST = "Onesmuson.mysql.pythonanywhere-services.com"
     MYSQL_DB = "Onesmuson$default"
 
-    # SQLAlchemy URI
+    # SQLAlchemy database URI
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+
+    # Avoid warnings
     SQLALCHEMY_TRACK_MODIFICATIONS = False
