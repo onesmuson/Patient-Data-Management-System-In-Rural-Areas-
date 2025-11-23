d1c9a1ef1bdc2f4a75c6352e55fb36bccc81fe33
onesmuson <masooonesmus1@gmail.com>
2025-11-23 17:28:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,17 +2,12 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
+    MYSQL_USER = os.getenv("MYSQL_USER", "Onesmuson")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "Admin1234")
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "Onesmuson.mysql.pythonanywhere-services.com")
+    MYSQL_DB = os.getenv("MYSQL_DB", "Onesmuson$default")
 
-    MYSQL_USER = os.getenv("MYSQL_USER")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    MYSQL_HOST = os.getenv("MYSQL_HOST")
-    MYSQL_DB = os.getenv("MYSQL_DB")
-
-    if all([MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_DB]):
-        SQLALCHEMY_DATABASE_URI = (
-            f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
-        )
-    else:
-        SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
-
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
