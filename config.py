82c5df4b95522c982c5a183533e3af3a5cd73461
onesmuson <masooonesmus1@gmail.com>
2025-11-23 14:40:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,15 @@
 import os
 
 class Config:
-    # Secret key for Flask sessions
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_default_secret_key")
+    SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")
 
-    # MySQL database settings
-    MYSQL_USER = "Onesmuson"  # PythonAnywhere username
-    MYSQL_PASSWORD = "Admin123"  # Your MySQL password
+    MYSQL_USER = "Onesmuson"
+    MYSQL_PASSWORD = "Admin123"
     MYSQL_HOST = "Onesmuson.mysql.pythonanywhere-services.com"
     MYSQL_DB = "Onesmuson$default"
 
-    # SQLAlchemy database URI
-    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+    )
 
-    # Avoid warnings
     SQLALCHEMY_TRACK_MODIFICATIONS = False
